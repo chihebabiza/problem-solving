26629e79e75a5b620b73ac2b8a90141e13892cc0
chiheb <chihababiza9@gmail.com>
2024-12-22 15:35:47 +0100

    4

---
+++ problems_level_1/4.cpp
@@ -0,0 +1,42 @@
+// read age and driver liscence from user and print hired or rejected
+
+#include <iostream>
+using namespace std;
+
+struct stInfo
+{
+    short age;
+    bool hasDriverLicence;
+};
+
+stInfo readInfo()
+{
+    stInfo info;
+    cout << "Enter your age\n";
+    cin >> info.age;
+    cout << "Do you has driver licence 0/1 \n";
+    cin >> info.hasDriverLicence;
+    return info;
+}
+
+bool isAccepted(stInfo info)
+{
+    if (info.age > 21 && info.hasDriverLicence)
+        return true;
+    else
+        return false;
+}
+
+void printResult(stInfo info)
+{
+    if (isAccepted(info))
+        cout << "Hired" << endl;
+    else
+        cout << "Rejected" << endl;
+}
+
+int main()
+{
+    printResult(readInfo());
+    return 0;
+}
