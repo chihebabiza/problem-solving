38287dff0d9dee6ca33a71f9ab39f1a2e95e4463
chiheb <chihababiza9@gmail.com>
2024-12-22 15:06:18 +0100

    1

---
+++ problems_level_1/1.cpp
@@ -0,0 +1,14 @@
+// print your name
+
+#include <iostream>
+using namespace std;
+
+void printName(string name)
+{
+    cout << "Your name is " << name << endl;
+}
+int main()
+{
+    printName("chiheb");
+    return 0;
+}
